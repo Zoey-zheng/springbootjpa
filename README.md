d87f8a8ef071f504ed8f36ec76fb2fa9bb1a21e5
zhengyingjun <13904942764@163.com>
2019-04-09 13:43:39 +0800

    修改pom、readme，增加注释

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # springbootjpa
 
-springboot + jpa 初始化程序
+#### 介绍
+springboot +jpa 测试程序
+#### 软件架构
+springboot 
+
+
+#### 安装教程
+
+1. xxxx
+2. xxxx
+3. xxxx
+
+#### 使用说明
+
+1. xxxx
+2. xxxx
+3. xxxx
+
+#### 说明
+使用jpa并添加自定义查询功能
+启动方式为war包，支持热部署
--- src/main/java/org/zhengyj/springbootjpa/ServletInitializer.java
+++ src/main/java/org/zhengyj/springbootjpa/ServletInitializer.java
@@ -2,6 +2,12 @@ package org.zhengyj.springbootjpa;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+/**
+ *
+ * @Author zhengyingjun
+ * @Description ：启动方式为war包 外置tomcat
+ * @Date 2019/4/9
+ **/
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
